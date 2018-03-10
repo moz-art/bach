57493b2bf4c6559ecb01d76b88774b7cb80d83f2
Yuren Ju <yurenju@gmail.com>
2018-03-10 09:42:48 +0800

    Implement volume up/down

--- src/containers/Conductor.js
+++ src/containers/Conductor.js
@@ -1,9 +1,20 @@
 import React, { PureComponent } from 'react';
 import Leap from 'leapjs';
+import styled from 'styled-components';
+
 import { PAGE_URL } from '../constants/Constant';
 import ServerAPI from '../ws/ServerAPI';
 
 const POSITION_THRESHOLD = 100;
+
+const Parts = styled.div`
+  display: flex;
+`;
+
+const Part = styled.div`
+  flex: auto;
+  background-color: ${props => props.current ? 'red' : 'white'};
+`;
 
 class Conductor extends PureComponent {
   constructor (props) {
@@ -11,7 +22,9 @@ class Conductor extends PureComponent {
 
     this.state = {
       meters: [],
-      bpm: 0
+      bpm: 0,
+      currentPart: 0,
+      volumes: [0.5, 0.5, 0.5, 0.5]
     };
   }
   componentDidMount () {
@@ -31,27 +44,50 @@ class Conductor extends PureComponent {
     let step = 0;
     Leap.loop(options, frame => {
       if (frame.hands.length > 0) {
-        const hand = frame.hands[0];
-        if (!startPosition) {
-          startPosition = hand.palmPosition;
-          previousTimestamp = frame.timestamp;
+        const hands = {};
+        frame.hands.forEach(hand => {
+          hands[hand.type] = hand;
+        });
+
+        if (hands.right) {
+          if (!startPosition) {
+            startPosition = hands.right.palmPosition;
+            previousTimestamp = frame.timestamp;
+          }
+
+          const currentPosition = hands.right.palmPosition;
+
+          const nextMax = Math.abs(currentPosition[axis[step]] - startPosition[axis[step]]);
+          if (nextMax > max) {
+            max = nextMax;
+          } else if (max - nextMax > POSITION_THRESHOLD) {
+            const duration = ((frame.timestamp - previousTimestamp) / 1000).toFixed(0);
+            const meters = this.state.meters.slice();
+            meters.push({ step: steps[step], duration });
+            const bpm = Math.floor(60 / (duration / 1000));
+            ServerAPI.setSpeed(bpm);
+            this.setState({meters, bpm});
+            startPosition = null;
+            max = 0;
+            step = (step + 1) % steps.length;
+          }
         }
 
-        const currentPosition = hand.palmPosition;
+        if (hands.left) {
+          const [x, y] = hands.left.palmPosition;
+          const direction = hands.left.palmNormal[1];
+          const volumes = this.state.volumes.slice();
+          const currentPart = Math.floor((x + 200) / 200 * this.state.volumes.length);
+          const nextVolume = Math.min(Math.max((y - 100) / 300, 0), 1);
+          const currentVolume = volumes[currentPart];
 
-        const nextMax = Math.abs(currentPosition[axis[step]] - startPosition[axis[step]]);
-        if (nextMax > max) {
-          max = nextMax;
-        } else if (max - nextMax > POSITION_THRESHOLD) {
-          const duration = ((frame.timestamp - previousTimestamp) / 1000).toFixed(0);
-          const meters = this.state.meters.slice();
-          meters.push({ step: steps[step], duration });
-          const bpm = Math.floor(60 / (duration / 1000));
-          ServerAPI.setSpeed(bpm);
-          this.setState({meters, bpm});
-          startPosition = null;
-          max = 0;
-          step = (step + 1) % steps.length;
+          // palm face to up
+          if (direction > 0 && nextVolume > currentVolume ||
+            direction <= 0 && nextVolume <= currentVolume) {
+            volumes[currentPart] = nextVolume;
+          }
+
+          this.setState({currentPart, volumes});
         }
       }
     });
@@ -67,6 +103,12 @@ class Conductor extends PureComponent {
       .map((meter, i) => <div key={i}>{meter.step}: {meter.duration}</div>);
   }
 
+  renderParts () {
+    return new Array(this.state.volumes.length).fill(0).map((_, index) => {
+      return <Part current={this.state.currentPart === index} key={index}>{this.state.volumes[index]}</Part>
+    });
+  }
+
   render() {
     if (!this.props.location.state || !this.props.location.state.code || !ServerAPI.ready) {
       // no code no show.
@@ -78,6 +120,9 @@ class Conductor extends PureComponent {
         <p>
           BPM: {this.state.bpm}
         </p>
+        <Parts>
+          {this.renderParts()}
+        </Parts>
         {this.renderMeters()}
       </div>
     );
